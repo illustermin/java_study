2b7befb8d8bb78a09476c9d0cce4b3f4ac62e274
illustermin <2002hongmj@naver.com>
2024-09-20 00:13:02 +0900

    Initial commit

---
+++ src/variable/Var2.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a;
+        a = 10;
+
+        System.out.println(a);
+    }
+}
+//변수 선언 -> 변수에 값 대입 -> 변수 값 읽기 -> 변수 값 변경
---
+++ src/variable/Var8.java
@@ -0,0 +1,16 @@
+//변수 타입 _ 정수는 int/long, 실수는 double 사용
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        //정수
+        byte b = 127; //-128 ~ 127
+        short s = 32767; // -32,768 ~ 32,767
+        int i = 2147483647; // -2147483648 ~ 2,147483647(약 20억)
+        long l  = 9223372036854775807L;
+
+        //실수
+        float f = 10.0f;
+        double d = 10.0;
+    }
+}
